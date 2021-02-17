c6662e3e09fc5ad999b78691c0c698855cf06c68
El Rafa <rafaelrosas19@gmail.com>
2021-02-16 19:29:10 -0600

    Added nav bar with links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import News from "./components/News";
+import Weather from "./components/Weather";
+import Sports from "./components/Sports";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+
+          <Link to="/news">News</Link>
+
+          <Link to="/weather">Weather</Link>
+
+          <Link to="/sports">Sports</Link>
+        </nav>
+
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/news">
+            <News />
+          </Route>
+          <Route path="/weather">
+            <Weather />
+          </Route>
+          <Route path="/sports">
+            <Sports />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,3 @@
+export default function Weather() {
+  return <h2>Weather</h2>;
+}
